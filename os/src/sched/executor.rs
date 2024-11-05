377fe6847bf43d1c53b1c98128471df7d9135355
Ya0rk <345024941@qq.com>
2024-11-05 21:07:29 +0800

    refactor executor

--- os/src/sched/executor.rs
+++ os/src/sched/executor.rs
@@ -1,83 +1,62 @@
-extern crate alloc;
-
-use crate::sync::mutex::NoInterruptSpinLock;
-use alloc::collections::VecDeque;
 use async_task::{
     Runnable, ScheduleInfo, Task, WithInfo
 };
 use core::future::Future;
+use core::hint::spin_loop;
+use core::sync::atomic::Ordering;
+use crossbeam_queue::SegQueue;
 
 struct TaskQueue {
-    queue: NoInterruptSpinLock<Option<VecDeque<Runnable>>>,
+    fifo: SegQueue<Runnable>,
+    prio: SegQueue<Runnable>,
 }
 
 impl TaskQueue {
-    pub const fn new() -> Self {
+    const fn new() -> Self {
         Self {
-            queue: NoInterruptSpinLock::new(None),
+            fifo: SegQueue::new(),
+            prio: SegQueue::new(),
         }
     }
-    pub fn init(&self) {
-        *self.queue.lock() = Some(VecDeque::new());
+
+    fn push_fifo(&self, runnable: Runnable) {
+        self.fifo.push(runnable);
     }
-    pub fn push(&self, runnable: Runnable) {
-        let mut lock = self.queue.lock();
-        lock.as_mut().unwrap().push_back(runnable);
+
+    fn push_prio(&self, runnable: Runnable) {
+        self.prio.push(runnable);
     }
-    pub fn push_preempt(&self, runnable: Runnable) {
-        self.queue.lock().as_mut().unwrap().push_front(runnable);
-    }
-    pub fn fetch(&self) -> Option<Runnable> {
-        self.queue.lock().as_mut().unwrap().pop_front()
+
+    fn take(&self) -> Option<Runnable> {
+        self.prio.pop().or_else(|| self.fifo.pop())
     }
 }
 
 static TASK_QUEUE: TaskQueue = TaskQueue::new();
 
-pub fn init() {
-    TASK_QUEUE.init();
-}
-
-/// Add a task into task queue
 pub fn spawn<F>(future: F) -> (Runnable, Task<F::Output>)
 where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
     let schedule = move |runnable: Runnable, info: ScheduleInfo| {
-        // TASK_QUEUE.push(runnable);
         if info.woken_while_running {
-            // i.e `yield_now()`
-            // log::error!("yield now");
-            TASK_QUEUE.push(runnable);
+            TASK_QUEUE.push_fifo(runnable);
         } else {
-            // i.e. woken up by some signal
-            TASK_QUEUE.push_preempt(runnable);
+            TASK_QUEUE.push_prio(runnable);
         }
     };
     async_task::spawn(future, WithInfo(schedule))
 }
 
-/// Return the number of the tasks executed
-pub fn run_until_idle() -> usize {
-    let mut n = 0;
+/// 开始执行任务
+pub fn run_executor() {
     loop {
-        if let Some(task) = TASK_QUEUE.fetch() {
-            // info!("fetch a task");
+        if let Some(task) = TASK_QUEUE.take() {
             task.run();
-            n += 1;
         } else {
-            break;
+            spin_loop();
         }
     }
-    n
 }
 
-#[allow(unused)]
-pub fn run_forever() -> ! {
-    loop {
-        if let Some(task) = TASK_QUEUE.fetch() {
-            task.run();
-        }
-    }
-}
